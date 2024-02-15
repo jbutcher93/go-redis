eb78a0973e714d3027f2b2aeedcb251fc4b1e531
jbutcher93 <1butcherjohn@gmail.com>
2024-02-15 17:16:44 -0500

    setup for k3d cluster

--- setup.sh
+++ setup.sh
@@ -1,8 +1,22 @@
-k3d cluster start
+#!/bin/bash
+
+k3d cluster create redis --wait
+kubectl config set-context k3d-redis
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 helm upgrade --install redis bitnami/redis
 kubectl apply -f manifests
-sleep 5
-kubectl port-forward svc/redis-master 6380:6379
-kubectl port-forward svc/redis-replicas 6379:6379
+while true; do
+    # Get the current status of the pod
+    STATUS=$(kubectl get deploy/redis-client -o jsonpath='{.status.readyReplicas}')
+
+    # Check if the status is "Running"
+    if [ "$STATUS" == "1" ]; then
+        echo "Pod is now running."
+        break
+    fi
+
+    # If the status is not "Running", wait for a few seconds before checking again
+    sleep 5
+done
+kubectl port-forward svc/redis-client 8080:80
